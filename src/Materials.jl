65bb81b5123017cbca03f1272a1134a14bbb7889
Rajesh Nakka <338rajesh@gmail.com>
2022-03-19 23:07:37 +0530

    Material data types are made more concrete

--- src/Materials.jl
+++ src/Materials.jl
@@ -1,26 +1,16 @@
 module Materials
+    __precompile__()
+    #
+    using LinearAlgebra
+    #
     using StaticArrays
     using Parameters
-    using LinearAlgebra
-    using FEPreProcessing
-    
-
-const Elastic_2DFEA = FEPreProcessing.Elastic_2DFEA
-const Elastic_3DFEA = FEPreProcessing.Elastic_3DFEA
-const Piezo_Electric_3DFEA = FEPreProcessing.Piezo_Electric_3DFEA
-const Piezo_Magnetic_3DFEA = FEPreProcessing.Piezo_Magnetic_3DFEA
-const Magneto_Electro_Elastic_3DFEA = FEPreProcessing.Magneto_Electro_Elastic_3DFEA
-const Thermal_Conductivity_3DFEA = FEPreProcessing.Thermal_Conductivity_3DFEA
-
-const Thermo_Elastic_3DFEA = FEPreProcessing.Thermo_Elastic_3DFEA
-const Thermo_Piezo_Electric_3DFEA = FEPreProcessing.Thermo_Piezo_Electric_3DFEA
-const Thermo_Piezo_Magnetic_3DFEA = FEPreProcessing.Thermo_Piezo_Magnetic_3DFEA
-const Thermo_Magneto_Electro_Elastic_3DFEA = FEPreProcessing.Thermo_Magneto_Electro_Elastic_3DFEA
-
     
     include("material_data_types.jl")
-    include("get_material_tensor.jl")
     include("material_lib.jl")
+    include("eval_engineering_constants.jl")
+
+    # const Material = Union{IsotropicMaterial, OrthotropicMaterial, TransverselyIsotropicMaterial} 
 
     export AbstractMaterial, Material
     export IsotropicMaterial, OrthotropicMaterial, TransverselyIsotropicMaterial
