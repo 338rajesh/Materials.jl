820802d67e3802373abce084aa936456faf268e5
Rajesh Nakka <338rajesh@gmail.com>
2022-12-12 19:01:09 +0530

    Modified units of Cij and dielectric constants of Piezo-Electric materials

--- src/material_lib.jl
+++ src/material_lib.jl
@@ -71,12 +71,12 @@ const Dglass = IsotropicMaterial(
 const epoxy_sample_1 = OrthotropicMaterial(
     tag="epoxy_sample_1",
     c_ij=Dict(
-        :C11 =>3.86,   :C12 => 2.57,   :C13 => 2.57,
-        :C22 =>3.86,   :C23 => 2.57,   :C33 => 3.86,
-        :C44 => 0.64,  :C55 => 0.64,   :C66 => 0.64,
+        :C11 => 3.86e09,  :C12 => 2.57e09,   :C13 => 2.57e09,
+        :C22 => 3.86e09,  :C23 => 2.57e09,   :C33 => 3.86e09,
+        :C44 => 0.64e09,  :C55 => 0.64e09,   :C66 => 0.64e09,
     ),
     e_ij = Dict(:e11=>0.0, :e21=>0.0, :e15=>0.0,),
-    eps_ij = Dict(:eps_11=>0.0797, :eps_22=>0.0797, :eps_33=>0.0797,),
+    eps_ij = Dict(:eps_11=>0.0797e-09, :eps_22=>0.0797e-09, :eps_33=>0.0797e-09,),
 )
 
 const epoxy_sample_2 = OrthotropicMaterial(
@@ -84,20 +84,20 @@ const epoxy_sample_2 = OrthotropicMaterial(
     c_ij=Dict(
         :C11 =>8e09,   :C12 => 4.4e09,   :C13 => 4.4e09,
         :C22 =>8e09,   :C23 => 4.4e09,   :C33 => 8e09,
-        :C44 => 1.8e09,  :C55 => 1.8e09,   :C66 => 1.8e09,
+        :C44 =>1.8e09, :C55 => 1.8e09,   :C66 => 1.8e09,
     ),
     e_ij = Dict(:e11=>0.0, :e21=>0.0, :e15=>0.0,),
-    eps_ij = Dict(:eps_11=>0.0372, :eps_22=>0.0372, :eps_33=>0.0372,),
+    eps_ij = Dict(:eps_11=>0.0372e-09, :eps_22=>0.0372e-09, :eps_33=>0.0372e-09,),
 )
 
 const PZT_5A = OrthotropicMaterial(
     tag="PZA-5A", c_ij=Dict(
-        :C11 =>111.0,   :C12 => 75.2,   :C13 => 75.2,
-        :C22 =>121.0,   :C23 => 75.4,   :C33 => 121.0,
-        :C44 => 22.8,   :C55 => 21.1,   :C66 => 21.1,
+        :C11 =>111.0e09,   :C12 => 75.2e09,   :C13 => 75.2e09,
+        :C22 =>121.0e09,   :C23 => 75.4e09,   :C33 => 121.0e09,
+        :C44 => 22.8e09,   :C55 => 21.1e09,   :C66 => 21.1e09,
     ),
     e_ij = Dict(:e11=>15.8, :e21=>-5.4, :e15=>12.3,),
-    eps_ij = Dict(:eps_11=>7.35, :eps_22=>8.11, :eps_33=>8.11,),
+    eps_ij = Dict(:eps_11=>7.35e-09, :eps_22=>8.11e-09, :eps_33=>8.11e-09,),
 )
 
 const PZT_7A = OrthotropicMaterial(
@@ -107,7 +107,7 @@ const PZT_7A = OrthotropicMaterial(
         :C44 => 35.8e09,   :C55 => 25.696e09,   :C66 => 25.696e09,
     ),
     e_ij = Dict(:e11=>9.52183, :e21=>-2.120582, :e15=>9.349593,),
-    eps_ij = Dict(:eps_11=>2.079, :eps_22=>4.065, :eps_33=>4.065,),
+    eps_ij = Dict(:eps_11=>2.079e-09, :eps_22=>4.065e-09, :eps_33=>4.065e-09,),
 )
 
 # SOURCE: https://doi.org/10.1142/S2010135X20500071
@@ -118,7 +118,7 @@ const PZT_4 = OrthotropicMaterial(
         :C44 => 30.6e09,   :C55 => 25.60e09,   :C66 => 25.60e09,
     ),
     e_ij = Dict(:e11=>15.1, :e21=>-5.20, :e15=>12.7,),
-    eps_ij = Dict(:eps_11=>5.61975, :eps_22=>6.4605, :eps_33=>6.4605,),
+    eps_ij = Dict(:eps_11=>5.61975e-09, :eps_22=>6.4605e-09, :eps_33=>6.4605e-09,),
 )
 
 # SOURCE: http://dx.doi.org/10.3390/met5020863
@@ -129,7 +129,7 @@ const PZT_6B = OrthotropicMaterial(
         :C44 => 54.0e09,   :C55 => 27.1e09,   :C66 => 27.1e09,
     ),
     e_ij = Dict(:e11=>7.1, :e21=>-0.9, :e15=>4.6,),
-    eps_ij = Dict(:eps_11=>3.4, :eps_22=>3.6, :eps_33=>3.6,),
+    eps_ij = Dict(:eps_11=>3.4e-09, :eps_22=>3.6e-09, :eps_33=>3.6e-09,),
 )
 
 
@@ -141,7 +141,7 @@ const PZT_8 = OrthotropicMaterial(
         :C44 =>124.0e09,   :C55 => 31.4e09,   :C66 => 31.4e09,
     ),
     e_ij = Dict(:e11=>13.8, :e21=>-4.0, :e15=>10.4,),
-    eps_ij = Dict(:eps_11=>5.1507, :eps_22=>7.9473, :eps_33=>7.9473,),
+    eps_ij = Dict(:eps_11=>5.1507e-09, :eps_22=>7.9473e-09, :eps_33=>7.9473e-09,),
 )
 
 const BaTiO3 = OrthotropicMaterial(
